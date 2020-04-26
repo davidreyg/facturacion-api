3f11e12e5b12168012d6700063d51ff31c78e0e2
David <neisserrey@upeu.edu.pe>
2020-04-26 08:52:19 -0500

    cambiando como el tutorial

--- config/database.php
+++ config/database.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Support\Str;
 
-
+$DATABASE_URL = parse_url(env('DATABASE_URL'));
+$host = $DATABASE_URL["host"] ?? null;
+$username = $DATABASE_URL["user"] ?? null;
+$password = $DATABASE_URL["pass"] ?? null;
+$database = substr($DATABASE_URL["path"], 1);
 return [
 
     /*
@@ -16,7 +20,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -68,11 +72,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $host,
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
